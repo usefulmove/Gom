f737f859b6b58d400d6d81dd9bd5951fb3426798
Duane <duane.edmonds@gmail.com>
2023-09-15 20:56:49 -0700

    ...

--- src/gum.el
+++ src/gum.el
@@ -5,8 +5,8 @@
 ;; Author: Duane <duane.edmonds@gmail.com>
 ;; Maintainer: Duane <duane.edmonds@gmail.com>
 ;; Created: August 25, 2023
-;; Modified: September 14, 2023
-;; Version: 0.0.10
+;; Modified: September 15, 2023
+;; Version: 0.0.11
 ;; Keywords: convenience data emulations extensions faces files frames languages lisp tools
 ;; Homepage: https://github.com/usefulmove/gum
 ;; Package-Requires: ((emacs "24.3"))
@@ -23,22 +23,41 @@
 ;; custom keybinding layer
 (map! :leader
       (:prefix "d"
-       :desc "Open (bash) terminal" "b" #'term
-       :desc "Evaluate last S-expression (sexp)" "e" #'eros-eval-last-sexp
-       :desc "Open file at link" "f" #'find-file-at-point
-       :desc "Insert the result of the evaluation of the last S-expression (sexp)" "i" #'gum-insert-eval-last-sexp
-       :desc "Insert org-mode link" "l" #'org-insert-link
-       :desc "Move current line to the end of the current buffer (eob)" "m" #'gum-move-line-to-eob
-       :desc "Open org-mode link" "o" #'org-open-at-point
-       :desc "Toggle visual-line-mode (word wrap)" "w" #'visual-line-mode))
+       :desc "Open (bash) terminal."
+             "b"
+             #'term
+       :desc "Evaluate last S-expression (sexp)."
+             "e"
+             #'eros-eval-last-sexp
+       :desc "Open file at link."
+             "f"
+             #'find-file-at-point
+       :desc "Insert the result of the evaluation of the last S-expression (sexp)."
+             "i"
+             #'gum-insert-eval-last-sexp
+       :desc "Insert org-mode link."
+             "l"
+             #'org-insert-link
+       :desc "Move current line to the end of the current buffer (eob)."
+             "m"
+             #'gum-move-line-to-eob
+       :desc "Open org-mode link."
+             "o"
+             #'org-open-at-point
+       :desc "Toggle visual-line-mode (word wrap)."
+             "w"
+             #'visual-line-mode))
 
 
 ;; set rainbow parens colors
-(custom-set-faces
-  '(rainbow-delimiters-depth-1-face ((t (:foreground "#fff670"))))
-  '(rainbow-delimiters-depth-2-face ((t (:foreground "#c792ea"))))
-  '(rainbow-delimiters-depth-3-face ((t (:foreground "#87ffaf"))))
-  '(rainbow-delimiters-depth-4-face ((t (:foreground "#00c0ff")))))
+(defun gum-color-rainbow-parens ()
+  (custom-set-faces
+    '(rainbow-delimiters-depth-1-face ((t (:foreground "#fff670"))))
+    '(rainbow-delimiters-depth-2-face ((t (:foreground "#c792ea"))))
+    '(rainbow-delimiters-depth-3-face ((t (:foreground "#87ffaf"))))
+    '(rainbow-delimiters-depth-4-face ((t (:foreground "#00c0ff"))))))
+
+(gum-color-rainbow-parens)
 
 '((((()))))
 
